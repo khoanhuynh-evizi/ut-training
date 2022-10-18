505d7be1b258223b7de8b5cac74af70a42077cd9
khoanhuynh-evizi <khoan.huynh@evizi.com>
2022-10-18 07:30:45 +0000

    feat: add app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StopWatch from './components/Stopwatch';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React StopWatch</h1>
+      <StopWatch />
+    </div>
+  );
+}
+
+export default App;
